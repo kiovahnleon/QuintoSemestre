5182d52e00a19eff8e809136b3a25c62420281b1
kiovahnleon <al20760618@ite.edu.mx>
2022-09-22 15:36:05 -0700

    small changes to buscador

--- LenguajesAutomatas/simpleBuscador/app.js
+++ LenguajesAutomatas/simpleBuscador/app.js
@@ -1,19 +1,20 @@
 const contain = document.querySelector('#resultados');
 let texto;
 const exp = /[\w\sáéíóú()+]*/;
-let alm = [];
+let arr = [];
+let contador = 0;
 
 async function buscar() {
   const respuesta = await fetch('https://smae-5f163-default-rtdb.firebaseio.com/SMAE.json');
   const data = await respuesta.json();
-  alm = data;
+  arr = data;
 }
 buscar();
 
 btn.onclick = (e) => {
   e.preventDefault();
   texto = document.querySelector('input').value;
-  const nombres = alm.map(data => data.Alimento)
+  const nombres = arr.map(data => data.Alimento)
   const busqueda = new RegExp(exp.source + texto + exp.source, 'gi');
   let match;
 
@@ -25,9 +26,12 @@ btn.onclick = (e) => {
     contain.appendChild(display);
     //document.getElementById("resultados").innerHTML += match[0] + "\n";
     console.log(match[0]);
+    contador++;
   }
 
 }
+
+
 
 
 //lo que puso la profe
